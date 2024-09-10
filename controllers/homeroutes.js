5ce28fdd85c19c096d3a8fe9a30a06294bd33edb
andrewyerm <yermakovichandrew@gmail.com>
2024-09-10 10:15:29 -0400

    start coding

---
+++ controllers/homeroutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User }],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render('home', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
